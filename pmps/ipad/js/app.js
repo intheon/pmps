ce6345f374eb03865634764fde250d045bc4b358
Ben S <allobon@gmail.com>
2015-10-21 14:01:29 +0100

    done

---
+++ pmps/ipad/js/app.js
@@ -0,0 +1,33 @@
+function grabTwitterData(rawJsonURL)
+{
+	$.ajax({
+		method: 	"GET",
+		url:   		rawJsonURL,
+		beforeSend: function(){
+			$(".loading").show();
+		},
+		success: 	function(response){
+			$(".loading").hide();
+			parseTwitterData(response);
+		}
+	});
+}
+
+function parseTwitterData(rawJson)
+{
+	var asObj = JSON.parse(rawJson);
+
+	for (i = 0; i < 9; i++)
+	{
+		var month = asObj[i].created_at.split(" ")[1];
+		var day = asObj[i].created_at.split(" ")[2];
+		var shortDate = month + " " + day;
+
+		$("#twitter-feed").append("<div class='twitter-item'>\
+			<div class='account-title'>@PMPSMedia&nbsp;</div>\
+			<div class='tweet-date'>"+ shortDate +"</div>\
+			<div class='tweet-body'>"+ asObj[i].text +"</div>\
+		</div>");
+	}
+
+}
